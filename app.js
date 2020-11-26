d4e6af41d6661cb469c530cfb2e7e71ee8f09d29
jtuuang <jt.uuang@gmail.com>
2020-11-26 10:38:26 +0800

    Modfiy app.js

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+let reqTimeStamp
+let reqTimeStampValues
+let method
+let url
+let reqDateNow
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(function (req, res, next) {
-  const method = req.method
-  const url = req.originalUrl
-  const reqTimeStamp = new Date
-  const reqTimeStampValues = [
+  method = req.method
+  url = req.originalUrl
+  reqDateNow = Date.now()
+  reqTimeStamp = new Date
+  reqTimeStampValues = [
     reqTimeStamp.getFullYear() + '-',
     reqTimeStamp.getMonth() + 1 + '-',
     reqTimeStamp.getDate() + ' ',
@@ -19,23 +25,29 @@ app.use(function (req, res, next) {
     reqTimeStamp.getSeconds(),
   ]
   next()
-  const resTimeStamp = new Date
-  console.log(`${reqTimeStampValues.join('')} | ${method} from ${url} | totol time: ${resTimeStamp.getMilliseconds() - reqTimeStamp.getMilliseconds()} milliseconds`)
 })
 
 app.get('/', (req, res) => {
+  const resDateNow = Date.now()
+  console.log(`${reqTimeStampValues.join('')} | ${method} from ${url} | totol time: ${resDateNow - reqDateNow} milliseconds`)
   res.render('index')
 })
 
 app.get('/new', (req, res) => {
+  const resDateNow = Date.now()
+  console.log(`${reqTimeStampValues.join('')} | ${method} from ${url} | totol time: ${resDateNow - reqDateNow} milliseconds`)
   res.render('new')
 })
 
 app.get('/:id', (req, res) => {
+  const resDateNow = Date.now()
+  console.log(`${reqTimeStampValues.join('')} | ${method} from ${url} | totol time: ${resDateNow - reqDateNow} milliseconds`)
   res.render('show')
 })
 
 app.post('/', (req, res) => {
+  const resDateNow = Date.now()
+  console.log(`${reqTimeStampValues.join('')} | ${method} from ${url} | totol time: ${resDateNow - reqDateNow} milliseconds`)
   res.send('新增一筆  Todo')
 })
 
